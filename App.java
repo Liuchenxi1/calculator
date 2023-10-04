f197d4c9899ef791bbe928383c3f98cd85478d46
Liu chenxi <shinnkiryu@gmail.com>
2023-10-04 13:23:34 -0500

    My first version of the calculator code

---
+++ App.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class App {
+
+    static final Scanner inputScanner = new Scanner(System.in);
+
+    private static String getString(String prompt) {
+        System.out.print(prompt);
+        String retrieve = inputScanner.nextLine();
+        return retrieve;
+    }
+    
+    private static double getDouble(String prompt) {
+        System.out.print(prompt);
+        double retrieve = inputScanner.nextDouble();
+        inputScanner.nextLine();
+        return retrieve;
+        
+    }
+    public static void main(String[] args) {
+        int tryout = 0;
+
+        while(True){ 
+            System.out.println("\nwhat you like to do?\n add\n substract\n multiply\n divide\n exit\n");
+            String choice = getString("Enter: ");
+
+            if (choice.equalsIgnoreCase("exit")) {
+                System.out.println("Ending the program");
+                System.exit(0);
+                break;
+            }
+
+            double num1 = getDouble("Enter the #1 number: ");
+            double num2 = getDouble("Enter the #2 number: ");
+
+            double result = 0;
+
+            if (choice.equalsIgnoreCase("add")) {
+                result = Calculator.add(num1, num2);
+                 
+            } 
+            else if (choice.equalsIgnoreCase("substract")) {
+                result = Calculator.substract(num1, num2); 
+                
+            } 
+            else if (choice.equalsIgnoreCase("multiply")) {
+                result = Calculator.multiply(num1, num2); 
+                
+            } else if (choice.equalsIgnoreCase("divide")) {
+                if (num2 != 0) {
+                    result = Calculator.divide(num1, num2); 
+                } else {
+                    System.out.println("Cannot divide by zero.");
+                }
+            } 
+            else {
+                System.out.println("Please enter a valid option.");
+            }
+
+            System.out.println("Result is: " + result);
+
+            // if(tryout %4 == 0){
+            //     String pickup = getString("Do you want to continue: yes or no? ");
+            //     if (!pickup.equalsIgnoreCase("yes")) {
+            //         System.out.println("Goodbye");
+            //         break;
+            //     }
+            }
+        }
+    }
+
+
---
+++ Calculator.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Calculator{
+
+    public static double add(double a, double b){
+        return a+b;
+    }
+
+    public static double substract(double a, double b){
+        return a-b;
+    }
+
+    public static double multiply(double a, double b){
+        return a*b;
+    }
+
+    public static double divide(double a, double b){
+        return a/b;
+    }
+}
+
+   // public static void main(String[] args){
+    //     // // final Scanner inputScanner = new Scanner(System.in);
+    //     // // System.out.println("Enter numbers: ");
+    //     // // int num = inputScanner.nextInt();
+    //     // // System.out.println("Num is "+num);
+    //     //     Scanner Scanner = new Scanner(System.in);
+    //     //     System.out.println("Enter numbers: ");
+    //     //     int num1 = Scanner.nextInt();
+    //     //     int num2 = Scanner.nextInt();
+    //     //     String result1 = String.format("you want to add %d and %d", num1,num2);
+    //     //     System.out.println(result1);
